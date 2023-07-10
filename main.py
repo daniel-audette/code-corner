061162f11a1b18166e69be4a9fb0213a41b89112
Daniel Mossie <daniel@audette.io>
2023-07-10 11:03:53 -0700

    demo commit

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ This is bad code!
 The purpose of this file is to demonstrate VSCode functionality
 '''
 
-foo = 3
+foo = 13
 bar = 5
 
 def add(a, b):
-    print("We are about to add two numbers")
-    sum_value = a + b
+    print("We are about to add two numbers!")
+    sum_value = a + b + b
+    print("Hello!")
     print(f"We have added {a} + {b} and the result is {sum_value}")
     return sum_value
 
 result = add(foo, bar)
-print(f"Result is: {result}")
